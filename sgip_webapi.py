835030c799174a2f73685a013816ed4157c5d1da
orangle <orangleliu@gmail.com>
2015-09-15 19:30:51 +0800

    http web api

---
+++ sgip_webapi.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+SGIP Server, Http server, client->server->sgip(联通)
+"""
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import urllib
+import httplib
+try:
+    import json
+except:
+    import simplejson as json
+
+HOST = '127.0.0.1'
+PORT = 30059
+
+def send_sms(mobile, content):
+    body = {"phone": mobile, "message": content}
+    jsonbody = json.dumps(body)
+    headers = {"User-Agent":"SMS-Sender/1.0",
+            'Content-Length':'%d'%len(jsonbody),
+            'Content-Type':'application/x-www-form-urlencoded',
+            }
+    try:
+        http_conn = httplib.HTTPConnection(host=HOST, port=PORT)
+        http_conn.request(method='POST', url='/smsapi', body=jsonbody, headers=headers)
+        http_response = http_conn.getresponse()
+        code = http_response.read()
+        try:
+            code = json.loads(code)['code']
+            return code
+        except:
+            return 3
+    except Exception, e:
+        print e
+        return 3
+    finally:
+        try:
+            http_conn.close()
+        except:
+            pass
+
+if __name__ == '__main__':
+    print send_sms('15010283629',u'您的手机验证码是：472380')
+
---
+++ sgip_webserver.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+SGIP Server, Http server, client->server->sgip(联通)
+"""
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+try:
+    import json
+except:
+    import simplejson as json
+
+
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+
+from sgip_client import send_sms
+
+class SmsAPI(Resource):
+    def render_GET(self, request):
+        return 'error'
+
+    def render_POST(self, request):
+        res = {"code": 2}
+        try:
+            jsondata = request.content.getvalue()
+            data = json.loads(jsondata)
+            phone = data.get('phone', "")
+            message = data.get('message', "")
+            if phone and message:
+                code = send_sms(str(phone), message)
+                res["code"] = code
+            else:
+                code = 3
+        except Exception as e:
+            res["code"] = 3
+
+        return json.dumps(res)
+
+root = Resource()
+root.putChild("smsapi", SmsAPI())
+factory = Site(root)
+reactor.listenTCP(30059, factory)
+reactor.run()
