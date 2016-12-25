d74c3c8ddba2f317947fba59d1c20621222eba46
orangle <orangleliu@gmail.com>
2016-12-25 17:52:46 +0800

    add sgip report

--- sgip_server.py
+++ sgip_server.py
@@ -3,25 +3,23 @@
 """
 SGIP server for receiving SGIP message from SMG
 文档 3.7.2.6
-
-用来接收上行短信的，不知道有没有短信回执功能
-什么是report命令？
-sp给smg可以一次发送多条信息，重试
 """
 
-from optparse import OptionParser
-import eventlet
-from sgip import *
-from binascii import *
-from sgip_client import *
 import logging
 import logging.handlers
+from binascii import *
 from datetime import datetime
+from optparse import OptionParser
+
+import eventlet
+
+from sgip import *
+from sgip_client import *
 
 # config logger
 log_name = 'sgip_server'
 logger = logging.getLogger(log_name)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 lh = logging.handlers.TimedRotatingFileHandler(
     log_name + '.log', when='midnight')
 lh.setLevel(logging.INFO)
@@ -30,7 +28,7 @@ lh.setFormatter(lf)
 logger.addHandler(lh)
 
 console = logging.StreamHandler()
-console.setLevel(logging.INFO)
+console.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s : %(message)s')
 console.setFormatter(formatter)
@@ -44,9 +42,8 @@ class SGIPProcessor(object):
 
     # receive data by specified size
     def __recv(self, size):
-        logger.info('...receiving raw data...')
         data = self.ssock.recv(size)
-        logger.info('recv data: %s', hexlify(data))
+        # logger.info('recv data: %s', hexlify(data))
         return data
 
     # send data
@@ -60,16 +57,16 @@ class SGIPProcessor(object):
     def __read_msg_header(self):
         logger.info('read msg header')
         raw_data = self.__recv(SGIPHeader.size())
-        logger.info('# header raw data: %s', hexlify(raw_data))
+        logger.debug('# header raw data: %s', hexlify(raw_data))
         if raw_data == '':
             return None
         header = SGIPHeader()
         header.unpack(raw_data)
-        logger.info('# msg len: %d', header.MessageLength)
-        logger.info('# command id: %d', header.CommandID)
-        logger.info('# sequence number: {0} {1} {2}'.format(
-            header.SequenceNumber[0], header.SequenceNumber[
-                1], header.SequenceNumber[2]))
+        # logger.info('# msg len: %d', header.MessageLength)
+        # logger.info('# command id: %d', header.CommandID)
+        # logger.info('# sequence number: {0} {1} {2}'.format(
+        #    header.SequenceNumber[0], header.SequenceNumber[
+        #        1], header.SequenceNumber[2]))
         return header
 
     # process SGIP message
@@ -78,10 +75,9 @@ class SGIPProcessor(object):
         while True:
             # read message header
             header = self.__read_msg_header()
-            if header == None:
+            if header is None:
                 logger.info('No header received, close the socket')
                 break
-
             # report userrpt trace_resp
             if header.CommandID == SGIPBind.ID:
                 self.__handle_bind_msg(header)
@@ -92,88 +88,81 @@ class SGIPProcessor(object):
                 break
             elif header.CommandID == SGIPReport.ID:
                 self.__handle_report_msg(header)
-
         self.ssock.close()
 
     # send SGIP message
     def __send_sgip_msg(self, sgip_msg, header):
-        logger.info('send sgip msg')
-        if sgip_msg == None or header == None:
+        if sgip_msg is None or header is None:
             return
         seq_num = header.SequenceNumber[:]
-        msgHeader = SGIPHeader(SGIPHeader.size() + sgip_msg.size(),
-                               sgip_msg.ID, seq_num)
-        sgip_msg.header = msgHeader
+        sgip_msg.header = SGIPHeader(SGIPHeader.size() + sgip_msg.size(),
+                                     sgip_msg.ID, seq_num)
         raw_data = sgip_msg.pack()
-        logger.info('# send raw data: %s', hexlify(raw_data))
+        logger.debug('# send raw data: %s', hexlify(raw_data))
         self.__send(raw_data)
 
     def __send_sgip_unbind_resp(self, header):
         logger.info('send unbind resp')
-        unbindRespMsg = SGIPUnbindResp()
-        self.__send_sgip_msg(unbindRespMsg, header)
+        unbind_resp_msg = SGIPUnbindResp()
+        self.__send_sgip_msg(unbind_resp_msg, header)
 
     def __handle_report_msg(self, header):
         logger.info('handler report msg')
         report_msg_len = header.MessageLength - header.size()
-        logger.info('report msg len: %d', deliver_msg_len)
         raw_data = self.__recv(report_msg_len)
-        logger.info('# report raw data: %s', hexlify(raw_data))
-        reportMsg = SGIPReport()
-        reportMsg.unpackBody(raw_data)
-        logger.info('report state: %s', self.State)
-        self.__send_sgip_msg(SGIPReportResp, header)
+        logger.debug('# report raw data: %s', hexlify(raw_data))
+        report_msg = SGIPReport()
+        report_msg.unpackBody(raw_data)
+        logger.info('report state: %s', report_msg.State)
+        self.__send_sgip_msg(SGIPReportResp(), header)
 
     def __handle_bind_msg(self, header):
         logger.info('handle bind msg')
         # continue to receive bind msg body
         raw_data = self.__recv(SGIPBind.size())
-        logger.info('# bind raw data: %s', hexlify(raw_data))
-        bindMsg = SGIPBind()
-        bindMsg.unpackBody(raw_data)
-        logger.info('login type: %d', bindMsg.LoginType)
-        logger.info('login name: %s', bindMsg.LoginName)
-        logger.info('login pwd: %s', bindMsg.LoginPassword)
+        logger.debug('# bind raw data: %s', hexlify(raw_data))
+        bind_msg = SGIPBind()
+        bind_msg.unpackBody(raw_data)
+        # logger.info('login type: %d', bind_msg.LoginType)
+        logger.debug('login name: %s', bind_msg.LoginName)
+        # logger.info('login pwd: %s', bind_msg.LoginPassword)
 
         # send Bind Resp
-        logger.info('send bind resp')
-        bindRespMsg = SGIPBindResp()
-        self.__send_sgip_msg(bindRespMsg, header)
+        logger.debug('send bind resp')
+        bind_resp_msg = SGIPBindResp()
+        self.__send_sgip_msg(bind_resp_msg, header)
 
     def __handle_deliver_msg(self, header):
         logger.info('handle deliver msg')
         # continue to receive deliver msg body
         deliver_msg_len = header.MessageLength - header.size()
-        logger.info(' deliver msg len: %d', deliver_msg_len)
         raw_data = self.__recv(deliver_msg_len)
-        logger.info('# deliver raw data: %s', hexlify(raw_data))
-        deliverMsg = SGIPDeliver()
-        deliverMsg.contentLength = deliver_msg_len - SGIPDeliver.size()
-        logger.info('msg content len: %d - SGIPDeliver origin size: %d' %
-                    (deliverMsg.contentLength, SGIPDeliver.size()))
-        deliverMsg.unpackBody(raw_data)
+        logger.debug('# deliver raw data: %s', hexlify(raw_data))
+        deliver_msg = SGIPDeliver()
+        deliver_msg.contentLength = deliver_msg_len - SGIPDeliver.size()
+        deliver_msg.unpackBody(raw_data)
         # send Deliver Resp
-        logger.info('send deliver resp')
-        deliverRespMsg = SGIPDeliverResp()
-        self.__send_sgip_msg(deliverRespMsg, header)
+        logger.debug('send deliver resp')
+        deliver_resp_msg = SGIPDeliverResp()
+        self.__send_sgip_msg(deliver_resp_msg, header)
         # process Deliver Msg content
-        self._process_deliver_content(deliverMsg)
+        self._process_deliver_content(deliver_msg)
 
     # do actual work according to the content of deliver message
-    def _process_deliver_content(self, deliverMsg):
-        logger.info('process deliver content')
-        if deliverMsg.UserNumber.find('86') == 0:
-            userNumber = deliverMsg.UserNumber[2:]
+    def _process_deliver_content(self, deliver_msg):
+        logger.debug('process deliver content')
+        if deliver_msg.UserNumber.find('86') == 0:
+            userNumber = deliver_msg.UserNumber[2:]
         else:
-            userNumber = deliverMsg.UserNumber
+            userNumber = deliver_msg.UserNumber
 
-        msg_content = deliverMsg.MessageContent.upper()
+        msg_content = deliver_msg.MessageContent
         logger.info('user number: %s msg content: %s' %
                     (userNumber, msg_content))
         status = ''
 
 
-def handleMsg(ssd):
+def handle_msg(ssd):
     logger.info(
         'client connected, start to handle request, .......................')
     processor = SGIPProcessor(ssd)
@@ -194,7 +183,7 @@ def main():
         try:
             new_sock, address = server.accept()
             logger.info("accepted %s %s", address, new_sock)
-            pool.spawn_n(handleMsg, new_sock)
+            pool.spawn_n(handle_msg, new_sock)
         except (SystemExit, KeyboardInterrupt):
             logger.info('server caught exception')
             break
